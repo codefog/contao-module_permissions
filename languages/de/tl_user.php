f5e82fa433912cb334486ead2d73e7fb0b13fb78
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2015-01-20 17:48:38 +0100

    Added the German language files (thanks to Didier Federer)

---
+++ languages/de/tl_user.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * module_permissions extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2013 Codefog
+ *
+ * @package module_permissions
+ * @author  Codefog <http://codefog.pl>
+ * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
+ * @author  Didier Federer <d.federer@designpilot.ch>
+ * @license LGPL
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_user']['feModules'] = array('Frontend Module', 'Hier können Sie den Zugriff auf die Frontend Module erlauben.');
